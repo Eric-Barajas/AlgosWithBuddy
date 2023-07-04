855338240dbf5e3eb98135be9a8e382bad8e645a
Eric-Barajas <barajaseric505@gmail.com>
2023-07-03 23:02:36 -0700

    first day back

---
+++ progressionAlgos/day1.js
@@ -0,0 +1,10 @@
+// Write a function greet that takes in a string argument, s, and returns the string "hey s". No tricks here. Run the tests to check your work.
+
+const greet = (s) => {
+    // todo
+    return `hey ${s}`;
+};
+
+console.log(greet("Chicken"));
+console.log(greet("Hey its me"));
+
