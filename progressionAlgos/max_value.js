21e2d4ac11cd77a197b7d616de6bbf8801b2583a
Eric-Barajas <barajaseric505@gmail.com>
2023-07-04 02:49:09 -0700

    tough

---
+++ progressionAlgos/max_value.js
@@ -0,0 +1,15 @@
+// Write a function, maxValue, that takes in array of numbers as an argument. The function should return the largest number in the array.
+// Solve this without using any built-in array methods.
+// You can assume that the array is non-empty.
+
+const maxValue = (nums) => {
+    let maximum = -Infinity;
+
+    for (let num of nums) {
+        if (num > maximum) maximum = num;
+    }
+    return maximum
+};
+
+console.log(maxValue([5, 6, -8, 2, 4])); // -> 6
+console.log(maxValue([4, 7, 2, 8, 10, 9])); // -> 10
